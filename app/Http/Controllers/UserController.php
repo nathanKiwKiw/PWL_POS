ac7d69ad148dcfe569eb42723b630e0590aa16ee
Renathan Anggoro Arry Wibisono <onethanggoro7@gmail.com>
2024-04-16 14:51:38 +0700

    JS4 prac2.2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -36,9 +36,16 @@ class UserController extends Controller
         // });
         // return view('user', ['data' => $user]);
 
-        $user = UserModel::findOr(20, ['username', 'nama'], function (){
-            abort(404);
-        });
+        // $user = UserModel::findOr(20, ['username', 'nama'], function (){
+        //     abort(404);
+        // });
+        // return view('user', ['data' => $user]);
+
+    // JS4 prac-2.2
+        $user = UserModel::findOrFail(1);
+        return view('user', ['data' => $user]);
+
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
         return view('user', ['data' => $user]);
     }
 }
