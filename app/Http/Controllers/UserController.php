07a1d7e363f2095286df1f09dd4ee60bb4f40c44
Renathan Anggoro Arry Wibisono <onethanggoro7@gmail.com>
2024-04-16 14:56:32 +0700

    JS4 prac2.4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -52,6 +52,36 @@ class UserController extends Controller
         $user = UserModel::where('level_id', 2)->count();
         dd($user);
         return view('user', ['data' => $user]);
+
+    // JS4 prac-2.4
+        $user = UserModel::firstOrCreate(
+            [
+                // 'username' => 'manager',
+                // 'nama' => 'Manager',
+
+                'username' => 'manager22',
+                'nama' => 'Manager Dua Dua',
+                'password' => Hash:make('12345'),
+                'level_id' => 2
+            ],
+        );
+
+        return view('user', ['data' => $user]);
+
+        $user = UserModel::firstOrNew(
+            [
+                // 'username' => 'manager',
+                // 'nama' => 'Manager',
+
+                'username' => 'manager33',
+                'nama' => 'Manager Tiga Tiga',
+                'password' => Hash:make('12345'),
+                'level_id' => 2
+            ],
+        );
+        $user->save();
+
+        return view('user', ['data' => $user]);
     }
 }
         
