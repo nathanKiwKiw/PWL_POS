80ba8fb2a019ae15381932ae645401e9ec75475e
Renathan Anggoro Arry Wibisono <onethanggoro7@gmail.com>
2024-04-17 01:09:29 +0700

    JS4 prac2.7

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -128,51 +128,45 @@ class UserController extends Controller
         // dd($user->wasChanged(['nama', 'username'])); // true
         
     // JS4 prac-2.6
-        $user = UserModel::all();
-        return view('user', ['data' => $user]);
+        // $user = UserModel::all();
+        // return view('user', ['data' => $user]);
 
-        public function tambah() {
-            return view('user_tambah');
-        }
+        // public function tambah() {
+        //     return view('user_tambah');
+        // }
 
-            public function tambah_simpan(Request $request) {
-                UserModel::create([
-                    'username' => $request->username,
-                    'nama' => $request->nama,
-                    'password' => Hash::make($request->password),
-                    'level_id' => $request->level_id
-                ]);
-                return redirect('/user');
-            }
+        //     public function tambah_simpan(Request $request) {
+        //         UserModel::create([
+        //             'username' => $request->username,
+        //             'nama' => $request->nama,
+        //             'password' => Hash::make($request->password),
+        //             'level_id' => $request->level_id
+        //         ]);
+        //         return redirect('/user');
+        //     }
 
-        public function ubah($id) {
-             $user = UserModel::find($id);
-             return view('user_ubah', ['data'=>$user]);
-        }
+        // public function ubah($id) {
+        //      $user = UserModel::find($id);
+        //      return view('user_ubah', ['data'=>$user]);
+        // }
 
-        public function ubah_simpan($id, Request $request) {
-            $user = UserModel::find($id);
+        // public function ubah_simpan($id, Request $request) {
+        //     $user = UserModel::find($id);
 
-            $user->username = $request->username;
-            $user->nama = $request->nama;
-            $user->level_id = $request->level_id;
+        //     $user->username = $request->username;
+        //     $user->nama = $request->nama;
+        //     $user->level_id = $request->level_id;
 
-            $user->save();
-            return redirect('/user');
-            }
+        //     $user->save();
+        //     return redirect('/user');
+        //     }
 
-        public function hapus($id) {
-            $user = UserModel::find($id);
-            $user->delete();
+        // public function hapus($id) {
+        //     $user = UserModel::find($id);
+        //     $user->delete();
 
-            return redirect('/user');
-        }
-    }
-}
-    
-
-        
-        
+        //     return redirect('/user');
+      
     // JS4 prac-2.7
         $user = UserModel::with('level')->get();
         dd($user);
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -25,7 +25,9 @@
             <td>{{ $d->username }}</td>
             <td>{{ $d->nama }}</td>
             <td>{{ $d->level_id }}</td>
-
+        {{-- JS4 prac-2.7 --}}
+            <td>{{ $d->level->level_kode }}</td>
+            <td>{{ $d->level->level_nama }}</td>
         {{-- JS4 prac-2.6 --}}
             <td><a href="{{ route('user.ubah', $d->user_id) }}">Ubah</a> | <a href="{{ route('user.hapus', $d->user_id) }}">Hapus</a></td>
         </tr>
